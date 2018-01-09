635567ebf704a91689d2645b4b456cc248662167
apehex <apehex@protonmail.com>
2018-01-09 21:03:13 +0100

    home draft

---
+++ hello.py
@@ -0,0 +1,19 @@
+import codecs
+import os
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+TEMPLATE_FOLDER = './inspirations/ai/'
+TEMPLATE_FILE = 'home.tpl'
+OUTPUT_FILE = os.path.join(TEMPLATE_FOLDER, TEMPLATE_FILE + '.html')
+
+env = Environment(
+        loader = FileSystemLoader(TEMPLATE_FOLDER),
+        autoescape = select_autoescape(['html', 'xml']))
+
+template = env.get_template(TEMPLATE_FILE)
+content = template.render()
+
+with codecs.open(OUTPUT_FILE, 'w', 'utf-8') as content_file:
+    content_file.write(content)
+
+#env.compile_templates(target='./inspirations/ai', extensions='tpl', zip=None)
--- inspirations/hello.py
+++
@@ -1,9 +0,0 @@
-from app import routes
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__, template_folder='ai')
-
-@app.route('/')
-def home():
-    return render_template('home.html')
