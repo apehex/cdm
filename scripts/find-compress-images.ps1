555731224d2cd1db37c99b64e788081c809150f0
apehex <apehex@protonmail.com>
2019-02-08 13:49:29 +0100

    Structure to match jekyll's conventions

--- scripts/find-compress-images.ps1
+++
@@ -1,6 +0,0 @@
-$files_to_compress = Get-ChildItem -Recurse | Where-Object { $_.Name -match '.*\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$' }
-[string[]] $paths_to_compress = @()
-ForEach ($file in $files_to_compress) {
-    $paths_to_compress += $file.FullName
-}
-Compress-Archive -Path $paths_to_compress -DestinationPath 'images.zip' -CompressionLevel Optimal
